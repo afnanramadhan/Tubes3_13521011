e4a89f317eabbf9172d5b4dfd34271027ede37e8
Lailabkn <13521016@std.stei.itb.ac.id>
2023-05-01 16:46:10 +0700

    up lg

--- src/frontend/pages/index.js
+++ src/frontend/pages/index.js
@@ -1,6 +1,31 @@
-import Head from 'next/head'
+// import Head from 'next/head'
+// import { SideBar } from "./SideBar";
+// import { TextBox } from './TextBox';
+// import styles from '../styles/Home.module.css';
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Head>
+//         <title>Halaman Utama</title>
+//         <link rel="icon" href="/favicon.ico" />
+//         <style>{`
+//           body {
+//             background-color: #393E46;
+//           }
+//         `}</style>
+//       </Head>
+//       <TextBox />
+//       <SideBar />
+//     </div>
+//   )
+// }
+
+
+import Head from 'next/head';
 import { SideBar } from "./SideBar";
 import { TextBox } from './TextBox';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -10,12 +35,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <style>{`
           body {
-            background-color: #393E46;
+            margin: 0;
+            padding: 0;
+            overflow: hidden; /* prevent scrolling */
           }
         `}</style>
       </Head>
-      <TextBox />
-      <SideBar />
+      <div className={`${styles.column} ${styles['sidebar']}`}>
+        <SideBar />
+      </div>
+      <div className={`${styles.column} ${styles['home']}`}>
+        <div className={styles['home-atas']}>
+        </div>
+        <div className={styles['home-bawah']}>
+          <TextBox />
+        </div>
+      </div>
+      {/* <div className={`${styles.column} ${styles['home-bawah']}`}></div> */}
     </div>
-  )
+  );
 }
+
