a97dfa7012a67153ae094f7fae172fcefc9ffb99
syrifaa <13521018@std.stei.itb.ac.id>
2023-05-02 20:43:09 +0700

    add database

--- src/backend/main.go
+++ src/backend/main.go
@@ -53,5 +53,54 @@ func main() {
 	rows, _ := db.Query("SELECT * FROM album ")
 	defer rows.Close()
 
+	// main calculator
+	// 	scanner := bufio.NewScanner(os.Stdin)
+	// 	scanner.Scan()
+	// 	text := scanner.Text()
+	// 	var regex, err = regexp.Compile(`[-+]?[0-9]*\.?[0-9]+([-+*/]?([0-9]*\.?[0-9]+))*`)
+	// 	if err != nil {
+	// 		fmt.Println(err.Error())
+	// 	}
+	// 	var hasil = regex.FindAllString(text, -1)
+	// 	if len(hasil) == 0 {
+	// 		fmt.Println("Sintaks persamaan tidak sesuai")
+	// 	} else {
+	// 		fmt.Println(hasil[0])
+	// 		lib.Calculator(hasil[0])
+	// 	}
+
+	// main kmp
+	// text := "ABCDEFGABCDFDF"
+	// pat := "BC"
+	// fmt.Println("Text: ", text)
+	// fmt.Println("Pattern: ", pat)
+	// temp := (lib.KmpMatch(text, pat))
+	// fmt.Println("Index: ", temp)
+	// if temp == -1 {
+	// 	fmt.Println("Not Matched")
+	// } else {
+	// 	fmt.Println("Matched")
+	// }
+
+	// main kalender
+	// scanner := bufio.NewScanner(os.Stdin)
+	// scanner.Scan()
+	// text := scanner.Text()
+	// var regex, err = regexp.Compile(`[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,4}`)
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// var hasil = regex.FindAllString(text, -1)
+	// if lib.IsCalValid(hasil[0]) {
+	// 	fmt.Println(lib.GetDay(hasil[0]))
+	// } else {
+	// 	fmt.Println("Invalid Date")
+	// }
+
+	// main bm
+	// fmt.Println("crazy brown fx", "own", lib.BM("crazy brown fx", "own"))
+	// fmt.Println("crazy brown fox", "fox", lib.BM("crazy brown fox", "fox"))
+	// fmt.Println("crazy brown fx", "fox", lib.BM("crazy brown fx", "fox"))
+	// fmt.Println("crazy brown fox", "fx", lib.BM("crazy brown fox", "fx"))
 
 }
